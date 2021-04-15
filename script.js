476f6b5ba6689bc2a1e7a5c8df4c9cd9d0dffe1c
Getu Gebre <getu.gebre.gg@gmail.com>
2021-04-14 23:30:10 -0500

    quiz functionality defined

--- script.js
+++ script.js
@@ -41,8 +41,11 @@ const quizData = [
         correct: 'b'
     }
 ]
-
+const submitBtn = document.getElementById("submitBtn")
 const questionE1 = document.getElementById('question')
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
@@ -50,17 +53,49 @@ const d_text = document.getElementById('d_text')
 
 
 let currentQuiz = 0;
+let score = 0
+
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers()
     const currentQuizData = quizData[currentQuiz]
     questionE1.innerHTML = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
-
-
-
-    currentQuiz++
 }
+
+function getSelected() {
+    let answer = undefined
+    answerEls.forEach(answerE1 => {
+        if(answerE1.checked){
+            answer = answerE1.id
+        }
+    })
+    return answer
+}
+function deselectAnswers(){
+    answerEls.forEach(answerE1 => {
+        answerE1.checked = false 
+    })
+}
+submitBtn.addEventListener('click', () => {
+
+    const answer  = getSelected() 
+    
+    if(answer){
+        if (answer === quizData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+        if(currentQuiz < quizData.length){
+            loadQuiz()
+        }else{
+                // SHOW RESULT
+            quiz.innerHTML = `<h2>You answered correctly ${score} / ${quizData.length} questions.</h2><button onclick=location.reload()>Reload</button>`
+        }
+    }
+    
+})
