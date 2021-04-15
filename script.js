a0c9ba94338617a05ec6138d317d9aa6e4c881f8
Getu Gebre <getu.gebre.gg@gmail.com>
2021-04-14 21:51:55 -0500

    question answer data created

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const quizData = [
+    {
+        question: 'How old is Getu?',
+        a: '40',
+        b: '39',
+        c: '41',
+        d: '42',
+        correct: 'c'
+    },
+    {
+        question: "What was the most used programming Language?",
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: "Who is the current president of USA?",
+        a: "Obama",
+        b: "Trump",
+        c: "Biden",
+        d: "Kamala",
+        correct: 'c'
+    },
+    {
+        question: "What does HTML stands for?",
+        a: "HyperText Markup Language",
+        b: 'Cascading Style Sheet',
+        c: 'JavaScript Object Notation',
+        d: 'Application Program Interface',
+        correct: 'a'
+    },
+    {
+        question: "What year was JS launched?",
+        a: '2020',
+        b: '1995',
+        c: '1996',
+        d: '1994',
+        correct: 'b'
+    }
+]
